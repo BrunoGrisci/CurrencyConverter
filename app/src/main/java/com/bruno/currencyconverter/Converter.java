8ad7816ba2284b115737147ce9f68b2070436934
BrunoGrisci <bigrisci@gmail.com>
2015-02-16 17:25:58 +0000

    Code improved.

--- app/src/main/java/com/bruno/currencyconverter/Converter.java
+++ app/src/main/java/com/bruno/currencyconverter/Converter.java
@@ -1,10 +1,12 @@
 package com.bruno.currencyconverter;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -56,20 +58,21 @@ public class Converter extends ActionBarActivity {
     ArrayList<String> operations = new ArrayList<String>();
     ArrayAdapter operationsAdapter;
 
-    int numCreated;
-    int numResumed;
+    int numCreated; //number of times the app is created in total
+    int numResumed; //number of times the app is resumed in total
 
-    int numCreatedLocal = 0;
-    int numResumedLocal = 0;
+    int numCreatedLocal = 0; //number of times the app was created
+    int numResumedLocal = 0; //number of times the app was resumed
 
-    boolean swapedFrom = false;
-    boolean swapedTo = false;
-    boolean swap = true;
+    boolean swapedFrom = false; //flag to control the insertion in the last exchanges list
+    boolean swapedTo = false; //flag to control the insertion in the last exchanges list
+    boolean swap = true; //flag for changing the swap button background
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Removes the action bar from the app
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         super.onCreate(savedInstanceState);
@@ -103,6 +106,7 @@ public class Converter extends ActionBarActivity {
 
         SharedPreferences savedRates = getSharedPreferences(getResources().getString(R.string.PREFS), 0);
 
+        //Control a possible overflow in the layout
         numCreated = Integer.parseInt(savedRates.getString(getResources().getString(R.string.numberCreated), "0")) + 1;
         if (numCreated > 10000) {
             numCreated = 1;
@@ -117,9 +121,12 @@ public class Converter extends ActionBarActivity {
         operationsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, operations);
         lastOperations.setAdapter(operationsAdapter);
 
+        //If the device is connected, update the exchange rates
         if (isOnline()) {
+            //For this app it was used the Yahoo Developer Network and yahoo.finance.exchange
             new RatesUpdate().execute(getResources().getString(R.string.updateURL));
         }
+        //If it is not connected, don't update and inform the user
         else {
             Context context = getApplicationContext();
             CharSequence text = getResources().getString(R.string.layoutUpdateFail);
@@ -143,7 +150,7 @@ public class Converter extends ActionBarActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 toCurrencyText.setText(exchangeCurrency(fromSpinner.getSelectedItem().toString(), toSpinner.getSelectedItem().toString(), fromCurrencyText.getDoubleValue()));
-                String fromValue = "0.0";
+                String fromValue = getResources().getString(R.string.layoutZero);
                 if (!fromCurrencyText.isEmpty()) {
                     fromValue = fromCurrencyText.getText().toString();
                 }
@@ -156,8 +163,6 @@ public class Converter extends ActionBarActivity {
                 operationsAdapter.notifyDataSetChanged();
                 swapedFrom = false;
                 swapedTo = false;
-                System.out.println(newOperation);
-
             }
         });
 
@@ -169,11 +174,12 @@ public class Converter extends ActionBarActivity {
                 toCurrencyText.setText(exchangeCurrency(fromSpinner.getSelectedItem().toString(), toSpinner.getSelectedItem().toString(), fromCurrencyText.getDoubleValue()));
 
                 if (!swapedFrom) {
-                    String fromValue = "0.0";
+                    String fromValue = getResources().getString(R.string.layoutZero);
                     if (!fromCurrencyText.isEmpty()) {
                         fromValue = fromCurrencyText.getText().toString();
                     }
 
+                    //Add new conversion to the list
                     String newOperation = fromSpinner.getSelectedItem().toString() + " " + fromValue + " = " + toSpinner.getSelectedItem().toString() + " " + toCurrencyText.getText().toString();
                     operations.add(0, newOperation);
                     if (operations.size() > 5) {
@@ -200,11 +206,12 @@ public class Converter extends ActionBarActivity {
                 toCurrencyText.setText(exchangeCurrency(fromSpinner.getSelectedItem().toString(), toSpinner.getSelectedItem().toString(), fromCurrencyText.getDoubleValue()));
 
                 if (!swapedTo) {
-                    String toValue = "0.0";
+                    String toValue = getResources().getString(R.string.layoutZero);
                     if (!toCurrencyText.isEmpty()) {
                         toValue = toCurrencyText.getText().toString();
                     }
 
+                    //Add new conversion to the list
                     String newOperation = fromSpinner.getSelectedItem().toString() + " " + fromCurrencyText.getText().toString() + " = " + toSpinner.getSelectedItem().toString() + " " + toValue;
                     operations.add(0, newOperation);
                     if (operations.size() > 5) {
@@ -225,20 +232,24 @@ public class Converter extends ActionBarActivity {
 
         swapButton = (Button) findViewById(R.id.swapButton);
         swapButton.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
             public void onClick(View v) {
 
                 String currencyName = fromSpinner.getSelectedItem().toString();
                 fromSpinner.setSelection(adapter.getPosition(toSpinner.getSelectedItem().toString()));
                 toSpinner.setSelection(adapter.getPosition(currencyName));
 
+                //Swaps the two selected currency
                 fromCurrencySymbol.setText(getResources().getString(getResources().getIdentifier(fromSpinner.getSelectedItem().toString(), "string", "com.bruno.currencyconverter")));
                 toCurrencySymbol.setText(getResources().getString(getResources().getIdentifier(toSpinner.getSelectedItem().toString(), "string", "com.bruno.currencyconverter")));
 
+                //Default value for new currency is 1.0
                 fromCurrencyText.setText(getResources().getString(R.string.layoutOne));
 
                 swapedFrom = true;
                 swapedTo = true;
 
+                //Changes the button background
                 if (swap) {
                     swapButton.setBackground(getResources().getDrawable(R.drawable.swapbuttonsmall));
                 }
@@ -257,6 +268,7 @@ public class Converter extends ActionBarActivity {
 
     @Override
     protected void onPause() {
+        //Saves the current status of the app
         super.onPause();
         SharedPreferences currentStatus = getSharedPreferences(getResources().getString(R.string.PREFS), 0);
         SharedPreferences.Editor editorStatus = currentStatus.edit();
@@ -268,6 +280,7 @@ public class Converter extends ActionBarActivity {
         editorStatus.putString(getResources().getString(R.string.numberResumed), String.valueOf(numResumed));
         editorStatus.commit();
 
+        //Saves the list of last operations
         SharedPreferences savedList = getSharedPreferences(getResources().getString(R.string.PREFS), 0);
         SharedPreferences.Editor editorList = savedList.edit();
         editorList.putString(getResources().getString(R.string.lastOparation0), operations.get(0));
@@ -282,6 +295,7 @@ public class Converter extends ActionBarActivity {
     protected void onResume() {
         super.onResume();
 
+        //Controls a possible overflow in the layout
         SharedPreferences currentStatus = getSharedPreferences(getResources().getString(R.string.PREFS), 0);
         numResumed = Integer.parseInt(currentStatus.getString(getResources().getString(R.string.numberResumed), getResources().getString(R.string.layoutZero))) + 1;
         if (numResumed > 10000) {
@@ -292,17 +306,16 @@ public class Converter extends ActionBarActivity {
         numResumedLocal = numResumedLocal + 1;
         numberResumedLocal.setText(" (" + String.valueOf(numResumedLocal) + ")");
 
+        //Loads
         numberCreated.setText(String.valueOf(Integer.parseInt(currentStatus.getString(getResources().getString(R.string.numberCreated), getResources().getString(R.string.layoutZero)))));
-
         final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.currency_name_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         fromCurrencyText.setText(currentStatus.getString(getResources().getString(R.string.fromValue), getResources().getString(R.string.layoutOne)));
         fromSpinner.setSelection(adapter.getPosition(currentStatus.getString(getResources().getString(R.string.fromCurrency), getResources().getString(R.string.GBP))));
         toSpinner.setSelection(adapter.getPosition(currentStatus.getString(getResources().getString(R.string.toCurrency), getResources().getString(R.string.USD))));
-
         lastUpdateText.setText(currentStatus.getString(getResources().getString(R.string.lastUpdateTime), getResources().getString(R.string.time)));
 
+        //Loads the list of last operations
         SharedPreferences savedOperations = getSharedPreferences(getResources().getString(R.string.PREFS), 0);
         operations.clear();
         operations.add(0, savedOperations.getString(getResources().getString(R.string.lastOparation0), getResources().getString(R.string.layoutEmpty)));
@@ -336,18 +349,18 @@ public class Converter extends ActionBarActivity {
     }
 
     public String exchangeCurrency(String fromCurrency, String toCurrency, double fromValue) {
-
+        //Apply the exchange rate to exchange between two currency
         String exchange = fromCurrency + toCurrency;
 
         SharedPreferences savedRates = getSharedPreferences(getResources().getString(R.string.PREFS), 0);
         String  oldExchangeRate = getResources().getString(getResources().getIdentifier(exchange, "string", "com.bruno.currencyconverter")).trim();
-        double exchangeRate = Double.valueOf(savedRates.getString(exchange, oldExchangeRate)).doubleValue();
+        double exchangeRate = Double.valueOf(savedRates.getString(exchange, oldExchangeRate));
         double exchangedValue = fromValue * exchangeRate;
-        String newValue = Double.toString(exchangedValue);
-        return newValue;
+        return Double.toString(exchangedValue);
     }
 
     public boolean isOnline() {
+        //Tests if the device is connected
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         return (netInfo != null && netInfo.isConnected());
@@ -360,10 +373,11 @@ public class Converter extends ActionBarActivity {
 
         @Override
         protected String doInBackground(String... params) {
+            //Download the updated exchange rates in background
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = null;
             Document doc;
-            String lastUpdateTime = "default";
+            String lastUpdateTime = "Default";
 
             try {
                 db = dbf.newDocumentBuilder();
@@ -371,22 +385,23 @@ public class Converter extends ActionBarActivity {
                 e.printStackTrace();
             }
             try {
-                doc = db.parse(new URL(params[0]).openStream());
-                lastUpdateTime = doc.getElementsByTagName("Time").item(doc.getElementsByTagName("Time").getLength() - 1).getTextContent() + " " + doc.getElementsByTagName("Date").item(doc.getElementsByTagName("Date").getLength() - 1).getTextContent();
-                for (int i = 0; i < doc.getElementsByTagName("Rate").getLength(); i++) {
-                    newRates.add(doc.getElementsByTagName("Rate").item(i).getTextContent());
-                    String id = doc.getElementsByTagName("Name").item(i).getTextContent().replace(" to ", "");
-                    rateIDs.add(id);
-                    System.out.println(rateIDs.get(i));
-                    System.out.println(newRates.get(i));
+                if (db != null) {
+                    doc = db.parse(new URL(params[0]).openStream());
 
+                    lastUpdateTime = doc.getElementsByTagName("Time").item(doc.getElementsByTagName("Time").getLength() - 1).getTextContent() + " " + doc.getElementsByTagName("Date").item(doc.getElementsByTagName("Date").getLength() - 1).getTextContent();
+                    for (int i = 0; i < doc.getElementsByTagName("Rate").getLength(); i++) {
+                        newRates.add(doc.getElementsByTagName("Rate").item(i).getTextContent());
+                        String id = doc.getElementsByTagName("Name").item(i).getTextContent().replace(" to ", "");
+                        rateIDs.add(id);
+                        System.out.println(rateIDs.get(i));
+                        System.out.println(newRates.get(i));
+
+                    }
                 }
 
                 return lastUpdateTime;
 
-            } catch (SAXException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
+            } catch (SAXException | IOException e) {
                 e.printStackTrace();
             }
 
@@ -408,6 +423,7 @@ public class Converter extends ActionBarActivity {
 
         @Override
         protected void onPostExecute(String result) {
+            //Update the exchange rates in the app
             SharedPreferences savedRates = getSharedPreferences(getResources().getString(R.string.PREFS), 0);
             SharedPreferences.Editor editor = savedRates.edit();
 
